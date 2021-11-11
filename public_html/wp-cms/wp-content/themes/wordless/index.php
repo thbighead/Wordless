ac4ec9820a44e82c8c33cd802a4a323d23132f3b
Thales Nathan <thcmatias@gmail.com>
2021-11-11 10:26:54 -0300

    Avoiding infinite refresh looping
    Now we just redirect to front URL if it6's different of WordPress Home URL (defined by APP_URL into .env file)

--- public_html/wp-cms/wp-content/themes/wordless/index.php
+++ public_html/wp-cms/wp-content/themes/wordless/index.php
@@ -2,12 +2,17 @@
 
 use Wordless\Helpers\Environment;
 
-$front_end_url = Environment::get('FRONT_END_URL');
+const URL_ROUTE_SEPARATOR = '/';
+
+$front_end_url = rtrim(Environment::get('FRONT_END_URL'), URL_ROUTE_SEPARATOR);
+$wp_home = rtrim(WP_HOME, URL_ROUTE_SEPARATOR);
 ?>
 
-<meta content="0; URL='<?= $front_end_url ?>''" http-equiv="refresh">
+<?php if ($front_end_url !== $wp_home): ?>
+    <meta content="0; URL='<?= $front_end_url ?>''" http-equiv="refresh">
 
-<!-- just in case the meta tag is not read properly, here is plan B: a JS redirect -->
-<script type="text/javascript">
-    window.location = '<?= $front_end_url ?>';
-</script>
+    <!-- just in case the meta tag is not read properly, here is plan B: a JS redirect -->
+    <script type="text/javascript">
+        window.location = '<?= $front_end_url ?>';
+    </script>
+<?php endif; ?>
