d10e2dbbe1ae607f4812f6821e2dcd4fdd4eefbc
Thales Matias <thales.matias@infobase.com.br>
2022-02-22 17:58:05 -0300

    Updating packages
    Also republishing config files

--- config/hookers.php
+++ config/hookers.php
@@ -3,9 +3,11 @@
 use Wordless\Hookers\BootControllers;
 use Wordless\Hookers\BootHttpRemoteCallsLog;
 use Wordless\Hookers\HideDiagnosticsFromUserRoles;
+use Wordless\Hookers\HooksDebugLog;
 
 return [
     BootControllers::class,
     BootHttpRemoteCallsLog::class,
     HideDiagnosticsFromUserRoles::class,
+    HooksDebugLog::class,
 ];
