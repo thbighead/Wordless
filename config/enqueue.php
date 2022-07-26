186bd9bae4bf8cb41aece3b738fa6dadae7f5e02
Thales Matias <thales.matias@infobase.com.br>
2022-07-26 11:55:34 -0300

    New config file

---
+++ config/enqueue.php
@@ -0,0 +1,9 @@
+<?php
+
+use Wordless\Abstractions\EnqueueableElements\EnqueueableScript;
+use Wordless\Abstractions\EnqueueableElements\EnqueueableStyle;
+
+return [
+    EnqueueableScript::configKey() => [],
+    EnqueueableStyle::configKey() => [],
+];
