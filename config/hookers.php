03e9e94c64015abd9ac14e5922c68d6c2b71f900
Thales Matias <thales.matias@infobase.com.br>
2022-11-04 11:10:19 -0300

    Adding boot Hooker to config

--- config/hookers.php
+++ config/hookers.php
@@ -4,6 +4,7 @@ use Wordless\Abstractions\Bootstrapper;
 use Wordless\Abstractions\WpSpeedUp;
 use Wordless\Hookers\AllowSvgUpload;
 use Wordless\Hookers\BootControllers;
+use Wordless\Hookers\BootCustomPostTypes;
 use Wordless\Hookers\BootHttpRemoteCallsLog;
 use Wordless\Hookers\DeferEnqueuedScripts;
 use Wordless\Hookers\EnqueueThemeEnqueueables;
@@ -19,6 +20,7 @@ return [
         ...WpSpeedUp::addAdditionalHooks(),
         AllowSvgUpload::class,
         BootControllers::class,
+        BootCustomPostTypes::class,
         BootHttpRemoteCallsLog::class,
         ChooseImageEditor::class,
         DeferEnqueuedScripts::class,
