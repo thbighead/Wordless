699f4471ad43df1dcb0a50e907028e16152a643c
Thales Nathan <thales.matias@infobase.com.br>
2024-09-13 21:14:41 -0300

    Many fixes
    - Adding missing declare strict types in console script
    - Introducing APP_SLUG env variable to fix the database host name
    - Fixing mariadb host container name
    - Using new APP_SLUG variable as COMPOSE_PROJECT_NAME value
    - Defining default Adminer server as mariadb throughout
    ADM_DEFAULT_SERVER env variable
    - Fixing workspace aliases.sh autocompletion (it was trying to do for
    Laravel projects)
    - Fixing crontab configuration by adding --no-tty flag to command

--- docker/workspace/aliases.sh
+++ docker/workspace/aliases.sh
@@ -58,7 +58,7 @@ alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
-alias art="php artisan"
+alias console="php console"
 alias artisan="php artisan"
 alias cdump="composer dump-autoload -o"
 alias composer:dump="composer dump-autoload -o"
@@ -154,13 +154,12 @@ function fs() {
 	fi;
 }
 
-# Add artisan autocomplete
-function _artisan()
+# Add console autocomplete
+function _console()
 {
 	COMP_WORDBREAKS=${COMP_WORDBREAKS//:}
-	COMMANDS=`php artisan --raw --no-ansi list | sed "s/[[:space:]].*//g"`
+	COMMANDS=`php console --raw --no-ansi list | sed "s/[[:space:]].*//g"`
 	COMPREPLY=(`compgen -W "$COMMANDS" -- "${COMP_WORDS[COMP_CWORD]}"`)
 	return 0
 }
-complete -F _artisan art
-complete -F _artisan artisan
+complete -F _console console
